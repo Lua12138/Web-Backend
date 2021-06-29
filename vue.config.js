7f06cc14e4ca0ab709f2b58477b4fb98cfda9a3c
gam2046 <gam2046@gmail.com>
2021-06-29 13:35:22 +0800

    typo

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://xxxx/device/', // 对应自己的接口
-                changeOrigin: true,
-                ws: true,
-                pathRewrite: {
-                    '^/api': '',
-                },
-            },
-        },
-    },
-    publicPath: './',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/my-project/'
+        : '/'
 }
